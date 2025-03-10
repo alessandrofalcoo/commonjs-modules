1b64569117106b6b67f9fb839cb27acffb201d94
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-10 15:12:30 +0100

    first commit

---
+++ Main/hobbies.js
@@ -0,0 +1,2 @@
+/* Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
+Esporta la funzione dal file. */
